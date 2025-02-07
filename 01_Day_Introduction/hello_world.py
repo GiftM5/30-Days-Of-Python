bb171a1dc6f57c98f5259dc0d51e5cead0902216
Mpho Mofokeng <mpmofokjhb024@studen.wethinkcode.co.za>
2025-02-07 14:16:06 +0200

    Completed step 1 of level 1

---
+++ 01_Day_Introduction/hello_world.py
@@ -0,0 +1,26 @@
+#EERCISE LEVEL 1
+
+a= 3
+b= 4
+def add():
+    print(a+b)
+def subtraction():
+    print(a-b)
+def multiplication():
+    print(a*b)
+def modulus():
+    print(a%b)
+def division():
+    print(a/b)
+def exponential():
+    print(a**b)
+def floor_division_operator():
+    print(a//b)
+
+add()
+subtraction()
+multiplication()
+modulus()
+division()
+exponential()
+floor_division_operator()
